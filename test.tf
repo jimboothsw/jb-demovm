d8586dc0de9628a2246eff131046ed766d4ea969
jimboothsw <94770864+jimboothsw@users.noreply.github.com>
2022-03-07 12:30:32 -0700

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,2 +1,12 @@
-// This is another test.
-// Another line
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.1.0"
+    }
+  }
+}
+
+provider "vsphere" {
+  # Configuration options
+}
